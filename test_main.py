2ec34cce5f05b655807f76e39fc32716bb2b657b
Kamil <zakiroffkam@yandex.ru>
2025-11-11 22:53:30 +0300

    added test and otchet

--- test_main.py
+++ test_main.py
@@ -1,9 +1,7 @@
 from fastapi.testclient import TestClient
 from main import app
 
-
 client = TestClient(app)
-
 
 
 def test_infer_positive_en():
@@ -24,7 +22,7 @@ def test_infer_negative_en():
 
 def test_infer_positive_ru():
     response = client.post("/infer",
-                           json={"text": "Привет! Этот фильм мне очень понравился, спасибо!"})
+                           json={"text": "Привет! Этот фильм мне очень понравился, спасибо!", "language": "ru"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'POSITIVE'
@@ -32,7 +30,15 @@ def test_infer_positive_ru():
 
 def test_infer_negative_ru():
     response = client.post("/infer",
-                           json={"text": "Фу, капец ты воняешь!"})
+                           json={"text": "Фу, капец ты воняешь!", "language": "ru"})
     json_data = response.json()
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
+
+
+def test_infer_unknown_language():
+    response = client.post("/infer",
+                           json={"text": "Cette langue n'est pas prise en charge", "language": "fr"})
+    json_data = response.json()
+    assert response.status_code == 400
+    assert json_data['detail'] == 'Unsupported language'
